bb10de958d1f10f464c8d832e75e141be9d2d327
0298Agung <85009595+0298Agung@users.noreply.github.com>
2021-05-29 20:29:34 +0700

    Add files via upload

---
+++ storage/framework/views/8d2b26845dade596a8c048a85f5819eb3c17b315.php
@@ -0,0 +1,171 @@
+<?php if (isset($component)) { $__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da = $component; } ?>
+<?php $component = $__env->getContainer()->make(App\View\Components\AppLayout::class, []); ?>
+<?php $component->withName('app-layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes([]); ?>
+     <?php $__env->slot('header'); ?> 
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <?php echo e(__('Profile')); ?>
+
+        </h2>
+     <?php $__env->endSlot(); ?>
+
+    <div>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <?php if(Laravel\Fortify\Features::canUpdateProfileInformation()): ?>
+                <?php
+if (! isset($_instance)) {
+    $html = \Livewire\Livewire::mount('profile.update-profile-information-form')->html();
+} elseif ($_instance->childHasBeenRendered('Vfufyvm')) {
+    $componentId = $_instance->getRenderedChildComponentId('Vfufyvm');
+    $componentTag = $_instance->getRenderedChildComponentTagName('Vfufyvm');
+    $html = \Livewire\Livewire::dummyMount($componentId, $componentTag);
+    $_instance->preserveRenderedChild('Vfufyvm');
+} else {
+    $response = \Livewire\Livewire::mount('profile.update-profile-information-form');
+    $html = $response->html();
+    $_instance->logRenderedChild('Vfufyvm', $response->id(), \Livewire\Livewire::getRootElementTagName($html));
+}
+echo $html;
+?>
+
+                <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'jetstream::components.section-border','data' => []]); ?>
+<?php $component->withName('jet-section-border'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes([]); ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+            <?php endif; ?>
+
+            <?php if(Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords())): ?>
+                <div class="mt-10 sm:mt-0">
+                    <?php
+if (! isset($_instance)) {
+    $html = \Livewire\Livewire::mount('profile.update-password-form')->html();
+} elseif ($_instance->childHasBeenRendered('tFBZF5X')) {
+    $componentId = $_instance->getRenderedChildComponentId('tFBZF5X');
+    $componentTag = $_instance->getRenderedChildComponentTagName('tFBZF5X');
+    $html = \Livewire\Livewire::dummyMount($componentId, $componentTag);
+    $_instance->preserveRenderedChild('tFBZF5X');
+} else {
+    $response = \Livewire\Livewire::mount('profile.update-password-form');
+    $html = $response->html();
+    $_instance->logRenderedChild('tFBZF5X', $response->id(), \Livewire\Livewire::getRootElementTagName($html));
+}
+echo $html;
+?>
+                </div>
+
+                <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'jetstream::components.section-border','data' => []]); ?>
+<?php $component->withName('jet-section-border'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes([]); ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+            <?php endif; ?>
+
+            <?php if(Laravel\Fortify\Features::canManageTwoFactorAuthentication()): ?>
+                <div class="mt-10 sm:mt-0">
+                    <?php
+if (! isset($_instance)) {
+    $html = \Livewire\Livewire::mount('profile.two-factor-authentication-form')->html();
+} elseif ($_instance->childHasBeenRendered('6WsGPlf')) {
+    $componentId = $_instance->getRenderedChildComponentId('6WsGPlf');
+    $componentTag = $_instance->getRenderedChildComponentTagName('6WsGPlf');
+    $html = \Livewire\Livewire::dummyMount($componentId, $componentTag);
+    $_instance->preserveRenderedChild('6WsGPlf');
+} else {
+    $response = \Livewire\Livewire::mount('profile.two-factor-authentication-form');
+    $html = $response->html();
+    $_instance->logRenderedChild('6WsGPlf', $response->id(), \Livewire\Livewire::getRootElementTagName($html));
+}
+echo $html;
+?>
+                </div>
+
+                <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'jetstream::components.section-border','data' => []]); ?>
+<?php $component->withName('jet-section-border'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes([]); ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+            <?php endif; ?>
+
+            <div class="mt-10 sm:mt-0">
+                <?php
+if (! isset($_instance)) {
+    $html = \Livewire\Livewire::mount('profile.logout-other-browser-sessions-form')->html();
+} elseif ($_instance->childHasBeenRendered('e2iUoPp')) {
+    $componentId = $_instance->getRenderedChildComponentId('e2iUoPp');
+    $componentTag = $_instance->getRenderedChildComponentTagName('e2iUoPp');
+    $html = \Livewire\Livewire::dummyMount($componentId, $componentTag);
+    $_instance->preserveRenderedChild('e2iUoPp');
+} else {
+    $response = \Livewire\Livewire::mount('profile.logout-other-browser-sessions-form');
+    $html = $response->html();
+    $_instance->logRenderedChild('e2iUoPp', $response->id(), \Livewire\Livewire::getRootElementTagName($html));
+}
+echo $html;
+?>
+            </div>
+
+            <?php if(Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures()): ?>
+                <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'jetstream::components.section-border','data' => []]); ?>
+<?php $component->withName('jet-section-border'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes([]); ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+
+                <div class="mt-10 sm:mt-0">
+                    <?php
+if (! isset($_instance)) {
+    $html = \Livewire\Livewire::mount('profile.delete-user-form')->html();
+} elseif ($_instance->childHasBeenRendered('zS3kX8H')) {
+    $componentId = $_instance->getRenderedChildComponentId('zS3kX8H');
+    $componentTag = $_instance->getRenderedChildComponentTagName('zS3kX8H');
+    $html = \Livewire\Livewire::dummyMount($componentId, $componentTag);
+    $_instance->preserveRenderedChild('zS3kX8H');
+} else {
+    $response = \Livewire\Livewire::mount('profile.delete-user-form');
+    $html = $response->html();
+    $_instance->logRenderedChild('zS3kX8H', $response->id(), \Livewire\Livewire::getRootElementTagName($html));
+}
+echo $html;
+?>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+ <?php if (isset($__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da)): ?>
+<?php $component = $__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da; ?>
+<?php unset($__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da); ?>
+<?php endif; ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php /**PATH C:\xampp\htdocs\IDV0298\resources\views/profile/show.blade.php ENDPATH**/ ?>
